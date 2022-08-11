95f0b3aa8da2297b00e17861ab348ab0c2d642e8
pedro imbrech <elprogramadorpedro@gmail.com>
2022-08-11 16:25:22 -0300

    git action commit

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get("/", (req, res) => {
   res.send("Olá, T5!");
 });
 
+app.get("/ordinary", (req, res) => {
+  res.send("Olá, T5 extraordinary!");
+});
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
