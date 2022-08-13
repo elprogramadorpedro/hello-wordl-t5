0a1049d744b4a27a3ff8d13307f84a4c9f054fd7
pedro imbrech <elprogramadorpedro@gmail.com>
2022-08-13 10:21:30 -0300

    github action  deploy brutal aquaman

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
 });
 
 
+app.get("/tutores", (req, res) => {
+  res.send("Olá, T5 tutores Pedro vini galdino guga!");
+});
+
+app.get("/tutoras", (req, res) => {
+  res.send("Olá, T5 tutoras lessa adriana!");
+});
+
 
 
 const port = process.env.PORT || 5000;
